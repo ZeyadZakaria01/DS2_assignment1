fa768aad72edaaa31dfc93e1725f6e5055b48729
iZarrios <eyad_zarrios2001@yahoo.com>
2022-03-20 18:53:44 +0200

    heap sort

--- heap_sort.py
+++ heap_sort.py
@@ -1,3 +1,39 @@
+# heapfiy function is to ernsure that when you insert a new elment in the heap
+# it will have its appropiate parent/child(s)
+
+def heapify(A, n, i):
+    root = i  # supposedly max elemnt in root
+    left = i * 2 + 1
+    right = i * 2 + 2
+
+    if left < n and A[left] > A[root]:
+        root = left
+    if right < n and A[right] > A[root]:
+        root = right
+
+    if root != i:
+        A[root], A[i] = A[i], A[root]
+        heapify(A, n, root)
+
+
+# we assume that we are inserting element A[i] each time in the heap
+
+def build_heap(A, n):
+    start = n//2 - 1
+    for i in range(start, -1, -1):
+        heapify(A, n, i)
+
+
 def heap_sort(A):
-    print("\n\nNOT IMPLEMENTED YET (HEAP SORT)\n\n")
-    return 0
+    n = len(A)
+    build_heap(A, n)
+
+    for i in range(n-1, 0, -1):
+        A[i], A[0] = A[0], A[i]  # ascending sorting
+        # A[i], A[n-1] = A[0], A[n-1] #descending order
+        heapify(A, i, 0)
+
+
+A = [4, 5, 0, 4500, 233]
+heap_sort(A)
+print(A)
